f5503e350f6b50dcbdc8532b5d4e52671db778b8
Bastien <bastien.felix@etu.univ-lyon1.fr>
2025-06-19 09:10:33 +0200

    le Z vérifie le format de fichier

---
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Zerra_Zaventura_Zeditor_FILE E:/Documents/SAE 201/ZTierr-ZAventura/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/Zerra_Zaventura_Zeditor.exe)
+set(__QT_DEPLOY_TARGET_Zerra_Zaventura_Zeditor_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Zerra_Zaventura_Zeditor_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,5 @@
-#include <QFileDialog>
 #include "mainwindow.h"
+#include <QFileDialog>
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -17,7 +17,23 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     QString Zfilename = QFileDialog::getOpenFileName(this, "ZOpen Ze Zfile");
-    QString Ztext = "<img src=\"" + Zfilename + "\" width=\"400\" height=\"500\" />";
-    ui->Django->setText(Ztext);
-}
 
+    int l = Zfilename.length();
+    QString ZfileFormat;
+
+    if (l >= 4) {
+        ZfileFormat = QString(Zfilename[l - 4]) + QString(Zfilename[l - 3]) + QString(Zfilename[l - 2]) + QString(Zfilename[l - 1]);
+
+        if (ZfileFormat == ".png" || ZfileFormat == ".jpg" || ZfileFormat == "jpeg") {
+            QString Ztext = "<img src=\"" + Zfilename + "\" width=\"400\" height=\"500\" />";
+            ui->Django->setText(Ztext);
+        } else {
+            ui->Django->setText("Le Z a volé les autres formats de fichiers. \n\nMerci de donner une image sous le bon format");
+        }
+    } else {
+        ui->Django->setText("Le Z a volé le fichier sélectionné. \n\nMerci de bien sélectionner un fichier.");
+    }
+
+
+
+}
