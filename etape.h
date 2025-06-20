0656fbbfc4f306d038d7cdd2d919d9a93e299f9b
Jérôme Tardivon <jerome.tardivon@gmail.com>
2025-06-20 17:45:35 +0200

    Push etape.h

---
+++ etape.h
@@ -0,0 +1,27 @@
+#ifndef ETAPE_H
+#define ETAPE_H
+#include <QMainWindow>
+#include <qcombobox.h>
+#include <qgridlayout.h>
+#include <qpushbutton.h>
+#include <qtextedit.h>
+#include <vector>
+
+
+class Etape : public QWidget
+
+{
+    Q_OBJECT
+public:
+    Etape(QPushButton* bt, QWidget* parent = nullptr);
+    QGridLayout* titleLayout;
+    std::vector<QGridLayout*> vectgrid;
+    QGridLayout* layout;
+    QGridLayout* layoutbt;
+    void build();
+    void addDialog();
+
+
+};
+
+#endif // ETAPE_H
